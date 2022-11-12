d6b8fc979f064a61fc2439a5e175a2ccce0ecba4
Milton Cavalcante <milton.cavalcante@itegra.com.br>
2022-11-12 17:41:52 -0300

    Configurações para dockerizar a aplicação e instalação do TypeORM

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -14,7 +14,6 @@ categoriesRoutes.post("/", (request, response) => {
 
 categoriesRoutes.get("/", (request, response) => {
   const { file } = request;
-  console.log(file);
   return listCategoriesController.handle(request, response);
 });
 
